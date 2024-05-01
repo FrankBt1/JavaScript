72c4f8a3cc2e477a7e34c8966274fa2022d2acd8
Frank <frankbautistahuacho@gmail.com>
2024-05-01 18:23:43 -0500

    cambios

--- example/scripts/index.js
+++ example/scripts/index.js
@@ -2,8 +2,19 @@ const selector = document.getElementById("mensaje");
 
 selector.innerHTML = "CONEXIÓN EXITOSA ENTRE HTML Y JS";
 
+
+
+
+
+
+/*const entrada = prompt("¿Qué prodcuto deseas comprar?");
+console.log(entrada);
+alert(entrada);*/
+
+
 const nombre = prompt("podrias indicarme tu nombre por favor?");
+let mensaje = "Bienvenido " + nombre;
 
-console.log("tu nombre es: " + nombre);
+console.log(mensaje);
+alert(mensaje);
 
-alert("bienvenido a mi página " + nombre + "!");
