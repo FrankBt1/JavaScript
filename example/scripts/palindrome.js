88ee0462d107ab49ac483a3e677d8db7e1d5188e
Frank <frankbautistahuacho@gmail.com>
2024-05-05 17:42:30 -0500

    palindrome

--- example/scripts/palindrome.js
+++ example/scripts/palindrome.js
@@ -1,4 +1,4 @@
-/*function esPalindrome(cadena) {
+function esPalindrome(cadena) {
   let caracteres = cadena.split("");// separa por caracteres
   let caracteresInvertidos = caracteres.reverse();// a los caracteres separados los invierte del final al inicio
   let cadenaInvertida = caracteresInvertidos.join("");// a los caracteres separados e invertidos los vuelve unir para verificar si la palabra es palindrome
@@ -9,9 +9,9 @@
   }
 }
 console.log(esPalindrome("hola"));
-console.log(esPalindrome("oso"));*/
+console.log(esPalindrome("oso"));
 
-function esPalindrome(cadena) {
+ function esPalindrome(cadena) {
   let rebanar = cadena.split("");
   let invertir = rebanar.reverse();
   let juntar = invertir.join("");
@@ -23,3 +23,4 @@ function esPalindrome(cadena) {
 }
 console.log(esPalindrome("dabale arroz a la zorra el abad"));
 console.log(esPalindrome("oso ese oso"));
+
