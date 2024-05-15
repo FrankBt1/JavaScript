63574cd271d4945e071028cba15b9d3e43bce6f0
Frank <frankbautistahuacho@gmail.com>
2024-05-15 12:48:07 -0500

    cambios en speed

--- speed/speed.js
+++ speed/speed.js
@@ -1,13 +1,28 @@
-const calcularVelocidad = (distancia, tiempo) => {
-  /*const distancia = 10
-    const tiempo = 2*/
-  const velocidad = distancia * tiempo;
-  return velocidad;
-};
+/*const calcularVelocidad = (distancia, tiempo) => {
+  //const velocidad = distancia * tiempo;
+  //return velocidad;
+  //return distancia * tiempo;
+};*/
 
-let velocidad1 = calcularVelocidad(5, 3);
-console.log("la velocidad de Juan es:" +velocidad1);
+//const calcularVelocidad = (distancia, tiempo) => distancia * tiempo;
+
+/*let velocidad1 = calcularVelocidad(5, 3);
+console.log("la velocidad de Juan es:" + velocidad1);
 let velocidad2 = calcularVelocidad(2, 3);
-console.log("la velocidad de pedro es:" +velocidad2);
+console.log("la velocidad de pedro es:" + velocidad2);
 let velocidad3 = calcularVelocidad(2, 3);
-console.log("la velocidad de pablo es:" +velocidad3);
+console.log("la velocidad de pablo es:" + velocidad3);*/
+
+
+
+
+function calcularvelocidad() {
+  const distancia = 20;
+  const tiempo = 2;
+  const velocidad = distancia / tiempo;
+  console.log("La velocidad del móvil es " + velocidad);
+}
+
+calcularvelocidad()
+
+
