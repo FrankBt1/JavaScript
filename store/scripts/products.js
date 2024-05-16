b3c183af8b0cc47957e23050caea3db89225aee9
Frank <frankbautistahuacho@gmail.com>
2024-05-15 20:36:26 -0500

    modificación de product.js

--- store/scripts/products.js
+++ store/scripts/products.js
@@ -18,7 +18,7 @@ class Product {
     }
     else{
       this.stock = this.stock - units ;
-      console.log("Unidades vendidas. Stock restante" + this.stock);
+      console.log(`Unidades vendidas: ${units}. Stock restante: ${this.stock}`);
     }
   }
 
@@ -35,8 +35,21 @@ const prod1 = new Product(123, "audifonos", 50, 20, ["imagen4.jpg"], true, "logi
 const prod2 = new Product(456, "mouse",  100,  10, ["imagen1.jpg", "imagen2.jpg","iamgen3"], true, "genius", ["white", "black"], "An amazing product", "Incluye impuesto País y percepción AFIP");
 const prod3 = new Product(456, "telefono", 75, 20,["imagen7.jpg", "imagen8.jpg","imagen9.jpg"], true, "nokia", ["blue", "green", "orange"], "A fantastic product", "Incluye impuesto País y percepción AFIP");
 const prod4 = new Product(14356, "laptop", 1200, 12, ["imagen6.png"], true, "samsung", ["orange", "purple"], "A wonderful product", "Incluye impuesto País y percepción AFIP");
-const prod5 = new Product(1598, "teclados", 90, 23, ["imagenes3.jpg"], true, "torito", ["pink", "silver"], "A stunning product", "Incluye impuesto País y percepción AFIP");
+const prod5 = new Product(1598, "teclados", 90, 12, ["imagenes3.jpg"], true, "torito", ["pink", "silver"], "A stunning product", "Incluye impuesto País y percepción AFIP");
 const prod6 = new Product(1898, "radio", 120, 20, ["imagenes5.jpg", false, "avalanch"], ["brown", "cyan"], "A remarkable product", "Incluye impuesto País y percepción AFIP")
 
 
 const products = [prod1, prod2, prod3, prod4,prod5, prod6]
+
+console.log(products[3]);
+
+//cambiando de proveedor del prod4
+prod4.setsupplier ="Nokia"
+console.log(prod4.getsupplier);
+
+
+//vendiendo unidades del prod5
+prod5.sellUnits (10)
+prod5.sellUnits (5)
+
+
